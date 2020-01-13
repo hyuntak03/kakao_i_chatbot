c84afd5df27b7c919eea2d34289f0510d9f052eb
hyuntak03 <hyuntak03.lee@gmail.com>
2020-01-13 19:44:33 +0900

    modified

--- app.js
+++ app.js
@@ -1370,7 +1370,10 @@ apiRouter.post('/switch', function (req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function () {
+// app.listen(port, function () {
+//     console.log('서버 실행중...');
+// });
+app.listen(9000, function () {
     console.log('서버 실행중...');
 });
 
